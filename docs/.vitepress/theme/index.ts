6c2a95324bee822998428a51b70fc778e41a4d99
MuFeng <admin@imufeng.cn>
2023-10-09 21:54:00 +0800

    测试引入 Google Analytics

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -5,5 +5,12 @@ import './green-theme.var.css'
 import './style.scss'
 // 时间线样式
 import "vitepress-markdown-timeline/dist/theme/index.css";
-
-export default BlogTheme
+import googleAnalytics from 'vitepress-plugin-google-analytics';
+export default {
+    ... BlogTheme,
+    enhanceApp: (ctx) => {
+        googleAnalytics({
+            id: 'G-38J2Q2X6HV',
+        })
+    }
+}
