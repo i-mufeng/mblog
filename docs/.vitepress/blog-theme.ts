a226e2c9f8aa4f46a5c17a4844563bad37204e27
MuFeng <admin@imufeng.cn>
2023-10-12 11:32:37 +0800

    完善部分细节 ，新增RSS订阅

--- docs/.vitepress/blog-theme.ts
+++ docs/.vitepress/blog-theme.ts
@@ -2,24 +2,24 @@
 import { getThemeConfig } from '@sugarat/theme/node';
 
 // 开启RSS支持（RSS配置）
-// import type { Theme } from '@sugarat/theme'
+import type { Theme } from '@sugarat/theme'
 
-// const baseUrl = 'https://sugarat.top'
-// const RSS: Theme.RSSOptions = {
-//   title: '粥里有勺糖',
-//   baseUrl,
-//   copyright: 'Copyright (c) 2018-present, 粥里有勺糖',
-//   description: '你的指尖,拥有改变世界的力量（大前端相关技术分享）',
-//   language: 'zh-cn',
-//   image: 'https://img.cdn.sugarat.top/mdImg/MTY3NDk5NTE2NzAzMA==674995167030',
-//   favicon: 'https://sugarat.top/favicon.ico',
-// }
+const baseUrl = 'https://www.imufeng.cn'
+const RSS: Theme.RSSOptions = {
+  title: 'Mblog - 沐风的个人博客',
+  baseUrl,
+  copyright: 'Mblog © 2021-2023 Created by 沐风',
+  description: '但愿岁月如客，来去皆从容。（后端开发技术交流）',
+  language: 'zh-cn',
+  image: baseUrl+'/logo.svg',
+  favicon: baseUrl+'/favicon.ico',
+}
 
 
 // 所有配置项，详见文档: https://theme.sugarat.top/
 const blogTheme = getThemeConfig({
   // 开启RSS支持
-  // RSS,
+  RSS,
   
   // 默认开启pagefind离线的全文搜索支持（如使用其它的可以设置为false）
   // 如果npx pagefind 时间过长，可以手动将其安装为项目依赖 pnpm add pagefind
